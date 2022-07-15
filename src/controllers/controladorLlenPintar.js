a7cee73009326c5784424c5aed8cf2e9fcf52c2f
alejandra garia <aljanddfj@4565.com>
2022-07-15 12:27:11 -0500

    clase 15 julio

---
+++ src/controllers/controladorLlenPintar.js
@@ -0,0 +1,49 @@
+ export function pintarProductos(productos){
+    
+    //1. Crear una referencia en memoria que traiga el htmal sobre el cual voy a pintar 
+    let fila=document.getElementById("fila")
+    fila.innerHTML=""
+
+    //2. recorrer el arreglo de datos 
+    productos.forEach(function(producto){
+        
+        // creamos lo que necesitamos
+        // creamos columna
+        let columna=document.createElement("div")
+        columna.classList.add("col")
+
+        let tarjeta=document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow","text-center")
+       
+        let foto=document.createElement("img")
+        foto.classList.add("img-fluid","w-100")
+        foto.src=producto.foto[0]
+
+
+        let nombreProducto=document.createElement("h3")
+        nombreProducto.textContent=producto.nombre
+
+        let precioProducto=document.createElement("h2")
+        precioProducto.classList.add("fw-bold")
+        precioProducto.textContent="$"+producto.precio+"$"
+
+        // deteccion de mouse 
+        columna.addEventListener("mouseover",function(evento){
+            foto.src=producto.foto[1]
+        })
+        
+        columna.addEventListener("mouseleave",function(evento){
+            foto.src=producto.foto[0]
+        })
+
+        tarjeta.appendChild(foto)
+        tarjeta.appendChild(nombreProducto)
+        tarjeta.appendChild(precioProducto)
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
+
+        
+})
+
+ }
+
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,72 @@
+ export let productosBD= [
+    {
+        nombre:"aguardiente antioqueño",
+        precio:55000,
+        cantidad:0,
+        foto:'https://firebasestorage.googleapis.com/v0/b/molinatiendatcc.appspot.com/o/guaro.webp?alt=media&token=b2290639-a3be-4c3a-9c7e-2667a0dc39e0',
+        descripcion:'aguardiente antioqueño, el aguardiente de la gente '
+    },
+   {
+        nombre:'Ron Medellin',
+        precio:45000,
+        cantidad:6,
+        foto:'https://elcorreodelanoche.com.co/wp-content/uploads/2020/02/ron-medellin-3-a%C3%B1os-litro.png',
+        descripcion:'el ron  mas fuerte'
+    },
+    {
+        nombre:'ron caldas',
+        precio:48000,
+        cantidad:4,
+        foto:'https://olimpica.vtexassets.com/arquivos/ids/606895/7702168218305.jpg?v=637626493136270000',
+        descripcion:'ron añejo de mejor sabor '
+    },
+    {
+        nombre:'heinenken',
+        precio:9000,
+        cantidad:20,
+        foto:'https://koyotebarbacoa.com/wp-content/uploads/2020/04/heineken-01.png',
+        descripcion:'cerveza elaborada en amsterdan '
+    },
+    {
+        nombre:'corona',
+        precio:7000,
+        cantidad:28,
+        foto:'https://distrilicoresdeltolima.com/wp-content/uploads/2020/06/589-5897300_corona-transparent-710ml-botella-de-corona-hd-png.png',
+        descripcion:'cerza elaborada abase de arroz'
+    },
+    {
+        nombre:'stella',
+        precio:11000,
+        cantidad:14,
+        foto:'https://restaurantetortelli.com/wp-content/uploads/2021/08/STELLA_ARTOIS.jpg',
+        descripcion:'cerveza de origen belga'
+    },
+    {
+        nombre:'jagermeister',
+        precio:65000,
+        cantidad:12,
+        foto:'https://www.eltiempo.com/uploads/2021/01/09/5ffa7ea8ea173.jpeg',
+        descripcion:'es de los pocos whisky con un sabor dulce'
+    },
+    {
+        nombre:'buchanans',
+        precio:130000,
+        cantidad:28,
+        foto:'https://revistaelconocedor.com/wp-content/uploads/2019/09/Buch-1024x683.jpg',
+        descripcion:'Whisky de origen irlandes '
+    },
+    {
+        nombre:'old parr',
+        precio:120000,
+        cantidad:23,
+        foto:'https://cdn.shopify.com/s/files/1/0335/3663/9116/products/unnamed_8_2491af3b-52d0-486e-b7f5-1c6540d70561_800x.jpg?v=1607175674',
+        descripcion:'Whisky de origen Alberbury, Reino Unido'
+    },
+    {
+        nombre:'red bull',
+        precio:11000,
+        cantidad:30,
+        foto:'https://media.istockphoto.com/photos/aluminium-can-of-red-bull-energy-drink-iced-background-picture-id537022544?k=20&m=537022544&s=612x612&w=0&h=r5OPbtcVPAxBkmnkyfczQwxfbFH9beInZeea0xxjoiM=',
+        descripcion:'Es un energizante fundado en los años 1980'
+    }
+]
